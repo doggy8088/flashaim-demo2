6fc24ea4ab4f7dfaa109e59e40abc48998453b57
Will <doggy.huang@gmail.com>
2018-08-02 14:31:39 +0800

    完成 Remember Me 功能

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgModel, NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
+interface LoginModel {
+  email?: string;
+  password?: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,19 +15,27 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
 
-  login: any = {};
-
+  login: LoginModel = {};
+  rememberMe = false;
   returnUrl: string;
 
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'];
+    this.login.email = localStorage.getItem('email');
+    this.rememberMe = !!localStorage.getItem('email');
   }
 
+  changeRememberMe(input: HTMLInputElement) {
+    if (!input.checked) {
+      localStorage.removeItem('email');
+    }
+  }
   doLogin(f: NgForm) {
   if (f.valid) {
       console.log(f.value);
+      localStorage.setItem('email', f.form.get('email').value);
       f.form.get('email').disable();
       f.form.get('password').disable();
         this.http.post('/api/login', f.value).subscribe((v: any) => {
